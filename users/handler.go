6b6ce8d5ba6b5b469561616ee84c377a7c254fdf
Arthur Schurhaus <arthurschur@icloud.com>
2025-05-08 13:06:18 -0300

    added get user by id route

--- users/handler.go
+++ users/handler.go
@@ -5,6 +5,8 @@ import (
 	"errors"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type UserHandler struct {
@@ -35,4 +37,34 @@ func (h *UserHandler) GetAllUsers(w http.ResponseWriter, r *http.Request) {
 		log.Printf("handler: error encoding response: %v", err)
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
+}
+
+func (h *UserHandler) GetUserById(w http.ResponseWriter, r *http.Request) {
+	var userId uuid.UUID
+	userIdStr := r.URL.Query().Get("id") // Assuming the URL is /users?id=<uuid>
+	userId, err := uuid.Parse(userIdStr)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	user, err := h.userService.GetUserById(r.Context(), userId)
+	if err != nil {
+		switch {
+		case errors.Is(err, ErrUserNotFound):
+			http.Error(w, "User not found", http.StatusNotFound)
+			return
+		default:
+			log.Printf("handler: error fetching user by ID: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		log.Printf("handler: error encoding response: %v", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
+
 }
